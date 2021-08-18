de41466154cb39635137645f8f421a944de5fa54
Mr.Cristofer <88551911+Cristofer6010@users.noreply.github.com>
2021-08-18 08:41:18 -0700

    Create faulty_calculator.py

---
+++ faulty_calculator.py
@@ -0,0 +1,20 @@
+print ("Enter first number")
+a = int(input())
+print ("What you want with these numbre")
+c = (input())
+print ("Enter second number")
+b = int(input())
+if c == "+" :
+    print (a+b)
+elif c == "-" :
+    print (a-b)
+elif a==2565 and b==3555 or c==2565*3555:
+    print (255555555555)
+elif c == "*" :
+    print (a*b)
+elif a==25025 and b==1594 or c==25025/1594:
+    print (6147259.61572312888727)
+elif a == 225 and b == 2271723 or c == 225 / 2271723:
+    print(467159727655.22)
+elif c == "/" :
+    print (a/b)
